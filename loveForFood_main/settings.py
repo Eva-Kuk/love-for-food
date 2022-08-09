e60f19f523c46fcf82e8668e290f6ba104ee8375
Evka K <kuklaewa@gmail.com>
2022-08-09 10:32:02 +0100

    add static folder template

--- loveForFood_main/settings.py
+++ loveForFood_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'loveForFood_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'loveForFood_main/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- loveForFood_main/views.py
+++ loveForFood_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'home.html')
