cc88a7ab9c1534437d6223ca988376b0d3222ae2
Evka K <kuklaewa@gmail.com>
2022-08-12 23:45:46 +0100

    add UserProfile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, UserProfile
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
@@ -11,3 +11,4 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = ()
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
--- accounts/models.py
+++ accounts/models.py
@@ -76,4 +76,20 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
     
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photos', blank=True, null=True)
+    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=6, blank=True, null=True)
+    latitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.user.email
