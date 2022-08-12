4be1950911b830aabebc76a1e97eb7ec06d6f3eb
Evka K <kuklaewa@gmail.com>
2022-08-13 00:11:12 +0100

    add configure Media Files

--- loveForFood_main/settings.py
+++ loveForFood_main/settings.py
@@ -128,6 +128,10 @@ STATICFILES_DIRS = [
     'loveForFood_main/static'
 ]
 
+# Media files configuration
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- loveForFood_main/urls.py
+++ loveForFood_main/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
