fd59c8424e985c038cc9370d1a7558992d64fc21
Evka K <kuklaewa@gmail.com>
2022-08-16 17:45:10 +0100

    add validation field errors

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,6 @@ from .models import User
 
 def registerUser(request):
     if request.method == 'POST':
-        print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
             # Create the user using the form
@@ -16,7 +15,6 @@ def registerUser(request):
             # user.set_password(password)
             # user.role = User.CUSTOMER
             # user.save()
-            
             # create the user using create_user method
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
@@ -26,8 +24,10 @@ def registerUser(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
-            print('User is created')
             return redirect('registerUser')
+        else:
+            print('invalid form')
+            print(form.errors)
     else:
         form = UserForm()
     context = {
