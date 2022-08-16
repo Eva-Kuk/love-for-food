1d532b0e6da4f4b833660082008902f43d8464e3
Evka K <kuklaewa@gmail.com>
2022-08-16 14:37:38 +0100

    add two solutions how to hash the password from form

--- accounts/views.py
+++ accounts/views.py
@@ -10,9 +10,23 @@ def registerUser(request):
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            # Create the user using the form
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+            
+            # create the user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
+            print('User is created')
             return redirect('registerUser')
     else:
         form = UserForm()
