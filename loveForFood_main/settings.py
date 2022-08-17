c977f9b9561891e8904f97a702dc4f494fe5a8a7
Evka K <kuklaewa@gmail.com>
2022-08-17 13:45:43 +0100

    add custom flash messages

--- loveForFood_main/settings.py
+++ loveForFood_main/settings.py
@@ -136,3 +136,8 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
