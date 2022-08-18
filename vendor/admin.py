a8570fc5282017df54b3bc26260803e3530c440e
Evka K <kuklaewa@gmail.com>
2022-08-18 20:57:47 +0100

    add VendorAdmin list display

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Vendor
 
-# Register your models here.
-admin.site.register(Vendor)
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+
+admin.site.register(Vendor, VendorAdmin)
