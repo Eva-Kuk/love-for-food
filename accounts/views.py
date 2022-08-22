17efaa3ec7c6929223cb78304e1ae0440a8b4acd
Evka K <kuklaewa@gmail.com>
2022-08-23 00:00:06 +0100

    add custom 403 page for restricting unauthorised users access the pages

--- accounts/views.py
+++ accounts/views.py
@@ -6,9 +6,26 @@ from .forms import UserForm
 from .models import User, UserProfile
 from django.contrib import messages, auth
 from .utils import detectUser
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+from django.core.exceptions import PermissionDenied
+
+
+# Restrict the vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied
+
+
+# Restrict the customer from accessing the vendor page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied
+
 
 def registerUser(request):
     if request.user.is_authenticated:
@@ -115,10 +132,12 @@ def myAccount(request):
 
 
 @login_required(login_url='login')
+@user_passes_test(check_role_customer)
 def custDashboard(request):
     return render(request, 'accounts/custDashboard.html')
 
 
 @login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     return render(request, 'accounts/vendorDashboard.html')
