7f1709beab9db90e24e4b1e60b0f26ed19c086e5
Evka K <kuklaewa@gmail.com>
2022-08-25 21:40:31 +0100

    add vendor approval/disapronal notification email from admin

--- accounts/utils.py
+++ accounts/utils.py
@@ -32,3 +32,10 @@ def send_verification_email(request, user, mail_subject, email_template):
     mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
     mail.send()
     
+    
+def send_notification(mail_subject, mail_template, context):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    message = render_to_string(mail_template, context)
+    to_email = context['user'].email
+    mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.send()
