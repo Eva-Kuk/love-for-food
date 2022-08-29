91ae2b0a64aa3d98236c47c770acc2d3aa716498
Evka K <kuklaewa@gmail.com>
2022-08-29 22:11:00 +0100

    fix context_processors lazy object error and loged out message

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,5 +1,8 @@
 from vendor.models import Vendor
 
 def get_vendor(request):
-    vendor = Vendor.objects.get(user=request.user)
+    try:
+        vendor = Vendor.objects.get(user=request.user)
+    except:
+        vendor = None
     return dict(vendor=vendor)
