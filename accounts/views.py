336e17ac9724472c4307304ae321692695cdf153
Evka K <kuklaewa@gmail.com>
2022-08-29 19:09:02 +0100

    add feature to load vendor Images, restaurant name, address dynamically

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.tokens import default_token_generator 
 
 from django.core.exceptions import PermissionDenied
-
+from vendor.models import Vendor
 
 # Restrict the vendor from accessing the customer page
 def check_role_vendor(user):
@@ -169,7 +169,11 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    return render(request, 'accounts/vendorDashboard.html')
+    vendor = Vendor.objects.get(user=request.user)
+    context = {
+        'vendor': vendor,
+    }
+    return render(request, 'accounts/vendorDashboard.html', context)
 
 
 def forgot_password(request):
