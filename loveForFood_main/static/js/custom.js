54be472f9e1c689bc29880e34bff67d03cb511bf
Evka K <kuklaewa@gmail.com>
2022-09-02 12:06:56 +0100

    add address components to fetch data

--- loveForFood_main/static/js/custom.js
+++ loveForFood_main/static/js/custom.js
@@ -18,8 +18,50 @@ function onPlaceChanged() {
     if (!place.geometry) {
         document.getElementById('id_address').placeholder = "Start typing...";
     } else {
-        console.log('place name=>', place.name)
+        //  console.log('place name=>', place.name)
     }
     // get the address components and assign them to the fields
-    console.log(place);
+    // console.log(place);
+    var geocoder = new google.maps.Geocoder()
+    var address = document.getElementById('id_address').value
+
+    geocoder.geocode({ 'address': address }, function(results, status) {
+        //console.log('results=>', results)
+        //console.log('status=>', status)
+        if (status == google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+
+            // console.log('lat=>', latitude);
+            // console.log('long=>', longitude);
+            // when chosen the address the lat and lng will be picked and assigned to their fields
+            $('#id_latitude').val(latitude);
+            $('#id_longitude').val(longitude);
+        }
+    });
+
+    // loop through the address components and assign other address data
+    console.log(place.address_components);
+    for (var i = 0; i < place.address_components.length; i++) {
+        for (var j = 0; j < place.address_components[i].types.length; j++) {
+            // get country
+            if (place.address_components[i].types[j] == 'country') {
+                $('#id_country').val(place.address_components[i].long_name);
+            }
+            // get county
+            if (place.address_components[i].types[j] == 'administrative_area_level_1') {
+                $('#id_state').val(place.address_components[i].long_name);
+            }
+            // get city
+            if (place.address_components[i].types[j] == 'locality') {
+                $('#id_city').val(place.address_components[i].long_name);
+            }
+            // get Eircode postal code
+            if (place.address_components[i].types[j] == 'postal_code') {
+                $('#id_pin_code').val(place.address_components[i].long_name);
+            } else {
+                $('#id_pin_code').val("")
+            }
+        }
+    }
 }
