c31a9283761eb13227decdf30ee03779f8d6a571
Evka K <kuklaewa@gmail.com>
2022-09-10 00:12:53 +0100

    add listing html for marketplace

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from vendor.models import Vendor
 
 def marketplace(request):
-    return render(request, 'marketplace/listing.html')
+    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
+    # counter rate
+    vendor_count = vendors.count()
+    context = {
+        'vendors': vendors, 
+        'vendor_count': vendor_count,
+    }
+    return render(request, 'marketplace/listing.html', context)
