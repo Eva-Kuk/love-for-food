3cc785f97cdc866ee78637fae1e5129382841476
Evka K <kuklaewa@gmail.com>
2022-09-12 22:27:34 +0100

    add display fooditems by category using prefetch related function

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from menu.models import Category, FoodItem
 from vendor.models import Vendor
+from django.db.models import Prefetch
 
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -15,7 +17,15 @@ def marketplace(request):
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
     
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset=FoodItem.objects.filter(is_available=True)
+        )
+    )
+    
     context = {
         'vendor':vendor,
+        'categories':categories,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
