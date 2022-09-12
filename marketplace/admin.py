40a77c8a19d3172c058c2267424a768bca83d646
Evka K <kuklaewa@gmail.com>
2022-09-12 23:37:28 +0100

    add Cart Model and admin for marketplace app

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cart
 
 # Register your models here.
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'fooditem', 'quantity', 'updated_at')
+    
+admin.site.register(Cart, CartAdmin)
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import User
+from menu.models import FoodItem
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fooditem = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return self.user
