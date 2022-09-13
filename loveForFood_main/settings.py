d342cd029f1ab3bea555b54b20d96470b91736ca
Evka K <kuklaewa@gmail.com>
2022-09-13 22:00:58 +0100

    add Cart counter Context Proccesor

--- loveForFood_main/settings.py
+++ loveForFood_main/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
                 'accounts.context_processors.get_google_api',
+                'marketplace.context_processors.get_cart_counter',
             ],
         },
     },
