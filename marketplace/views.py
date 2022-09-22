0d5d5d09f16135af6437fb9b10a05711bb42c23f
Evka K <kuklaewa@gmail.com>
2022-09-22 23:58:24 +0100

    add search functionality setup Search Bar

--- marketplace/views.py
+++ marketplace/views.py
@@ -114,3 +114,6 @@ def delete_cart(request, cart_id):
                 return JsonResponse({'status': 'Failed', 'message': 'This Cart Item does not exist!'})
         else:
             return JsonResponse({'status':'Failed', 'message': 'Invalid request!'})
+
+def search(request):
+    return HttpResponse('Search Page')
