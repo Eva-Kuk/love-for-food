0d0df5b37a34c0fc60475e26dfd9e07c86c66a7d
Evka K <kuklaewa@gmail.com>
2022-09-22 09:59:44 +0100

    add check if cart is empty

--- loveForFood_main/static/js/custom.js
+++ loveForFood_main/static/js/custom.js
@@ -147,8 +147,28 @@ $(document).ready(function() {
                 } else {
                     $('#cart_counter').html(response.cart_counter['cart_count']);
                     swal(response.status, response.message, "success")
+
+                    removeCartItem(0, cart_id);
+                    checkEmptyCart();
                 }
             }
         })
     })
+
+    // delete the cart element if the quantity is 0
+    function removeCartItem(cartItemQty, cart_id) {
+        if (cartItemQty <= 0) {
+            // remove the cart item element
+            document.getElementById("cart_item-" + cart_id).remove()
+
+        }
+    }
+
+    // Check if the cart is empty
+    function checkEmptyCart() {
+        var cart_counter = document.getElementById('cart_counter').innerHTML
+        if (cart_counter == 0) {
+            document.getElementById("empty-cart").style.display = "block";
+        }
+    }
 });
