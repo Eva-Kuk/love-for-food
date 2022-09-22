7bd529b21885d347c5c5f74c6b6d65905b40ed47
Evka K <kuklaewa@gmail.com>
2022-09-22 13:12:13 +0100

    add Get Cart amounts function to get subtotal and grand total

--- loveForFood_main/settings.py
+++ loveForFood_main/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'accounts.context_processors.get_vendor',
                 'accounts.context_processors.get_google_api',
                 'marketplace.context_processors.get_cart_counter',
+                'marketplace.context_processors.get_cart_amounts',
             ],
         },
     },
--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -14,3 +14,19 @@ def get_cart_counter(request):
         except:   
             cart_count = 0
     return dict(cart_count=cart_count)
+
+
+def get_cart_amounts(request):
+    subtotal = 0
+    tax = 0
+    grand_total = 0
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+        for item in cart_items:
+            fooditem = FoodItem.objects.get(pk=item.fooditem.id)
+            subtotal += (fooditem.price * item.quantity) # subtotal = subtotal + (fooditem.price * item.quantity)
+            
+        grand_total = subtotal + tax
+        print(subtotal)
+        print(grand_total)
+    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
