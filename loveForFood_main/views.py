ed2919bb013b6eb852ae3bb12486d6e9f4ce10d5
Evka K <kuklaewa@gmail.com>
2022-10-21 22:07:00 +0100

    add show nearby restaurants on homepage feature

--- loveForFood_main/views.py
+++ loveForFood_main/views.py
@@ -3,8 +3,21 @@ from django.http import HttpResponse
 
 from vendor.models import Vendor
 
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D #Distance "D" is a shortcut for "Distance"
+from django.contrib.gis.db.models.functions import Distance
+
 def home(request):
-    vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
+    if 'lat' in request.GET:
+        lat = request.GET.get('lat')
+        lng = request.GET.get('lng')
+        pnt = GEOSGeometry('POINT(%s %s)' % (lng, lat))
+        vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=1000))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
+        
+        for v in vendors:
+            v.kms = round(v.distance.km, 1)
+    else:
+        vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)[:8]
     context = {
         'vendors': vendors, 
     }
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,17 +1,18 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .context_processors import get_cart_counter, get_cart_amounts
 from menu.models import Category, FoodItem
+
 from vendor.models import Vendor
 from django.db.models import Prefetch
-from django.http import HttpResponse, JsonResponse
 from .models import Cart
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 
 from django.contrib.gis.geos import GEOSGeometry
-from django.contrib.gis.measure import D, Distance #"D" is a shortcut for "Distance"
+from django.contrib.gis.measure import D #Distance "D" is a shortcut for "Distance"
 from django.contrib.gis.db.models.functions import Distance
-from django.shortcuts import redirect
+
 
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
