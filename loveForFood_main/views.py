01f6ce23325946956b69f5b58b55ea3beb9d7a4a
Evka K <kuklaewa@gmail.com>
2022-10-21 23:18:38 +0100

    add set the location in session variable

--- loveForFood_main/views.py
+++ loveForFood_main/views.py
@@ -7,11 +7,25 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D #Distance "D" is a shortcut for "Distance"
 from django.contrib.gis.db.models.functions import Distance
 
-def home(request):
-    if 'lat' in request.GET:
+def get_or_set_current_location(request):
+    if 'lat' in request.session:
+        lat = request.session['lat']
+        lng = request.session['lng']
+        return lng, lat
+    elif 'lat' in request.GET:
         lat = request.GET.get('lat')
         lng = request.GET.get('lng')
-        pnt = GEOSGeometry('POINT(%s %s)' % (lng, lat))
+        request.session['lat'] = lat
+        request.session['lng'] = lng
+        return lng, lat
+    else:
+        return None
+    
+def home(request):
+    if get_or_set_current_location(request) is not None:
+      
+        pnt = GEOSGeometry('POINT(%s %s)' % (get_or_set_current_location(request)))
+        
         vendors = Vendor.objects.filter(user_profile__location__distance_lte=(pnt, D(km=1000))).annotate(distance=Distance("user_profile__location", pnt)).order_by("distance")
         
         for v in vendors:
