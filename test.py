9e1a16f8859b41f9a870795b770c2ebc2d8df909
Evka K <kuklaewa@gmail.com>
2022-10-22 00:37:15 +0100

    add create oppening model

---
+++ test.py
@@ -0,0 +1,10 @@
+from datetime import time
+# [('12:00 AM', '12:00 AM), ('12:30 AM', '12:30 AM')....('11:30 PM', '11:30 PM')]
+# h=hour, m=minute
+
+# for h in range(0, 24):
+#     for m in(0, 30):
+#         print(time(h, m).strftime('%I:%M %p'))
+
+# t = [(time(h, m).strftime('%I:%M %p'), time(h, m).strftime('%I:%M %p')) for h in range(0, 24) for m in(0, 30)]
+# print(t)
--- vendor/admin.py
+++ vendor/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Vendor
+from .models import Vendor, OpeningHour
 
 
 class VendorAdmin(admin.ModelAdmin):
     list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
     list_display_links = ('user', 'vendor_name')
     list_editable = ('is_approved', )
+    
+    
+class OpeningHourAdmin(admin.ModelAdmin):
+    list_display = ('vendor', 'day', 'from_hour', 'to_hour')
 
 admin.site.register(Vendor, VendorAdmin)
+admin.site.register(OpeningHour, OpeningHourAdmin)
