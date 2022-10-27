2b35e126bd84fa658a7140801729c4a19c0c940e
Evka K <kuklaewa@gmail.com>
2022-10-27 23:35:10 +0100

    fix unique together vendor issue and make migrations

--- vendor/models.py
+++ vendor/models.py
@@ -58,7 +58,7 @@ class OpeningHour(models.Model):
     
     class Meta:
         ordering = ('day', '-from_hour')
-        unique_together = ('day', 'from_hour', 'to_hour') 
+        unique_together = ('vendor', 'day', 'from_hour', 'to_hour') 
         
     def __str__(self):
         return self.get_day_display()
