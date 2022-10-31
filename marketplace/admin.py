00434b95d6137e0010174bb9b8f554e7f58e2b07
Evka K <kuklaewa@gmail.com>
2022-10-31 22:32:23 +0000

    add dynamic tax module create model

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Cart
+from .models import Cart, Tax
 
 # Register your models here.
 class CartAdmin(admin.ModelAdmin):
     list_display = ('user', 'fooditem', 'quantity', 'updated_at')
     
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('tax_type', 'tax_percentage', 'is_active')
+    
 admin.site.register(Cart, CartAdmin)
+admin.site.register(Tax, TaxAdmin)
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from accounts.models import User
 from menu.models import FoodItem
@@ -12,3 +13,14 @@ class Cart(models.Model):
     
     def __unicode__(self):
         return self.user
+    
+    
+class Tax(models.Model):
+    tax_type = models.CharField(max_length=20, unique=True)
+    tax_percentage = models.DecimalField(decimal_places=2, max_digits=4, verbose_name='Tax Percentage(%)')
+    is_active = models.BooleanField(default=True)
+    class Meta:
+        verbose_name_plural = 'tax'
+
+    def __str__(self):
+        return self.tax_type
