016c964c7377eaf7fdbdfdf6be78bb0e2ad4f1ac
Evka K <kuklaewa@gmail.com>
2022-11-01 00:52:18 +0000

    add calculate tax amount

--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -1,4 +1,4 @@
-from .models import Cart
+from .models import Cart, Tax
 from menu.models import FoodItem
 
 def get_cart_counter(request):
@@ -20,11 +20,32 @@ def get_cart_amounts(request):
     subtotal = 0
     tax = 0
     grand_total = 0
+    tax_dict = {}
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
         for item in cart_items:
             fooditem = FoodItem.objects.get(pk=item.fooditem.id)
             subtotal += (fooditem.price * item.quantity) # subtotal = subtotal + (fooditem.price * item.quantity)
             
+        get_tax = Tax.objects.filter(is_active=True)    
+        for i in get_tax:
+            tax_type = i.tax_type
+            tax_percentage = i.tax_percentage
+            tax_amount = round((tax_percentage * subtotal)/100, 2)
+            print(tax_type, tax_percentage, tax_amount)
+            tax_dict.update({tax_type: {tax_percentage : tax_amount}})
+            
+    #   tax = 0        
+    #   for key in tax_dict.values():
+    #     for x in key.values():
+    #         tax = tax + x
+            
+        tax = sum(x for key in tax_dict.values() for x in key.values())        
+        print('tax==>', tax)
+            
+        # print(tax_dict)
+            
+        # {'Standard VAT':{'23.00':'15.55'}}
+            
         grand_total = subtotal + tax
-    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
+    return dict(subtotal=subtotal, tax=tax, grand_total=grand_total, tax_dict=tax_dict)
