33ef7ac03a008d09fcbcc10c25df17850ae81fec
Evka K <kuklaewa@gmail.com>
2022-11-01 00:53:44 +0000

    remove coments and print for testing

--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -32,20 +32,8 @@ def get_cart_amounts(request):
             tax_type = i.tax_type
             tax_percentage = i.tax_percentage
             tax_amount = round((tax_percentage * subtotal)/100, 2)
-            print(tax_type, tax_percentage, tax_amount)
-            tax_dict.update({tax_type: {tax_percentage : tax_amount}})
-            
-    #   tax = 0        
-    #   for key in tax_dict.values():
-    #     for x in key.values():
-    #         tax = tax + x
-            
+            tax_dict.update({tax_type: {tax_percentage : tax_amount}})   
         tax = sum(x for key in tax_dict.values() for x in key.values())        
-        print('tax==>', tax)
-            
-        # print(tax_dict)
-            
-        # {'Standard VAT':{'23.00':'15.55'}}
-            
+
         grand_total = subtotal + tax
     return dict(subtotal=subtotal, tax=tax, grand_total=grand_total, tax_dict=tax_dict)
