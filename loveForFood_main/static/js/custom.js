937a42154c86192538494b736b28abf4e3b1b4ac
Evka K <kuklaewa@gmail.com>
2022-11-02 23:18:55 +0000

    update Tax in the frontend your order cart, add min height style

--- loveForFood_main/static/js/custom.js
+++ loveForFood_main/static/js/custom.js
@@ -93,7 +93,7 @@ $(document).ready(function() {
                     // subtotal, tax and grand total
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
                 }
@@ -133,7 +133,7 @@ $(document).ready(function() {
 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -169,7 +169,7 @@ $(document).ready(function() {
 
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total']
                     )
 
@@ -197,11 +197,21 @@ $(document).ready(function() {
     }
 
     // apply cart amounts
-    function applyCartAmounts(subtotal, tax, grand_total) {
+    function applyCartAmounts(subtotal, tax_dict, grand_total) {
         if (window.location.pathname == '/cart/') {
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#total').html(grand_total)
+
+            console.log(tax_dict)
+            for (key1 in tax_dict) {
+                console.log(tax_dict[key1])
+                for (key2 in tax_dict[key1]) {
+                    // console.log(tax_dict[key1][key2])
+                    $('#tax-' + key1).html(tax_dict[key1][key2])
+
+                }
+
+            }
         }
     }
 
