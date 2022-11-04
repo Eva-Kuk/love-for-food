1ba80ba1a235e6ce419008779f33a15048e36696
Evka K <kuklaewa@gmail.com>
2022-11-04 00:25:39 +0000

    add Profile Form and User Form

--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserProfileForm, UserInfoForm
 
+@login_required(login_url='login')
 def cprofile(request):
-    return render(request, 'customers/cprofile.html')
+    profile_form = UserProfileForm()
+    user_form = UserInfoForm()
+    
+    context = {
+        'profile_form': profile_form,
+        'user_form': user_form,
+    }
+    return render(request, 'customers/cprofile.html', context)
