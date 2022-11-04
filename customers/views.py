98b840aa01ace5420ce0f7c39e32063aed1fb3df
Evka K <kuklaewa@gmail.com>
2022-11-04 01:04:21 +0000

    add prepopulate customer Forms

--- customers/views.py
+++ customers/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm, UserInfoForm
+from accounts.models import UserProfile
 
 @login_required(login_url='login')
 def cprofile(request):
-    profile_form = UserProfileForm()
-    user_form = UserInfoForm()
+    profile = get_object_or_404(UserProfile, user=request.user)
+    profile_form = UserProfileForm(instance=profile)
+    user_form = UserInfoForm(instance=request.user)
     
     context = {
         'profile_form': profile_form,
