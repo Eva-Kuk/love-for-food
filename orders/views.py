3fb7213f010482e6c187e5862eeffa87e3d292e2
Evka K <kuklaewa@gmail.com>
2022-11-13 22:47:51 +0000

    add store Payment Object and Update Order

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from marketplace.models import Cart
 from marketplace.context_processors import get_cart_amounts
 from .forms import OrderForm
-from .models import Order
+from .models import Order, Payment
 import simplejson as json
 from .utils import generate_order_number
 
@@ -50,4 +50,35 @@ def place_order(request):
     return render(request,  'orders/place_order.html')
 
 def payments(request):
+    # Check if the request is Ajax or not
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method == 'POST':  
+        # Store the payment details in the payment model
+        order_number = request.POST.get('order_number')
+        transaction_id = request.POST.get('transaction_id')
+        payment_method = request.POST.get('payment_method')
+        status = request.POST.get('status')
+        
+        order = Order.objects.get(user=request.user, order_number=order_number)
+        payment = Payment(
+            user = request.user,
+            transaction_id = transaction_id,
+            payment_method = payment_method,
+            amount = order.total,
+            status = status
+        )
+        payment.save()
+        # Update the order model
+        order.payment = payment
+        order.is_ordered = True
+        order.save()
+        return HttpResponse('Saved!')
+        # Move the Cart items to ordered food model
+        
+        # Send order confirmation email to the customer
+        
+        # Send order received email to the vendor
+        
+        # Clear the Cart if the payment is success
+        
+        # Return back to Ajax with the status success or failure
     return HttpResponse('Payments view')
