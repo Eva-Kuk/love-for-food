0bbb82031bc4a781c8978244761aef94588b4140
Evka K <kuklaewa@gmail.com>
2022-11-17 22:07:49 +0000

    add total Vendor Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -180,7 +180,13 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    return render(request, 'accounts/vendorDashboard.html')
+    vendor = Vendor.objects.get(user=request.user)
+    orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('-created_at')
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+    }
+    return render(request, 'accounts/vendorDashboard.html', context)
 
 
 def forgot_password(request):
