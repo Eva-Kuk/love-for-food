13fabdac521094b84d989de47980e49eb0fb754a
Evka K <kuklaewa@gmail.com>
2022-11-17 19:17:33 +0000

    add Order Detail Page finished

--- customers/views.py
+++ customers/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm, UserInfoForm
 from accounts.models import UserProfile
 from django.contrib import messages
-from orders.models import Order
+from orders.models import Order, OrderedFood
+import simplejson as json
 
 @login_required(login_url='login')
 def cprofile(request):
@@ -41,4 +42,21 @@ def my_orders(request):
 
 
 def order_detail(request, order_number):
-    return render(request, 'customers/order_detail.html')
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_food = OrderedFood.objects.filter(order=order)      
+        subtotal = 0
+        for item in ordered_food:
+            subtotal += (item.price * item.quantity)
+        tax_data = json.loads(order.tax_data)
+        context = {
+            'order': order,
+            'ordered_food': ordered_food,
+            'subtotal': subtotal,
+            'tax_data': tax_data,
+            
+        }
+        return render(request, 'customers/order_detail.html', context)
+    except:
+        return redirect('customer')
+    
