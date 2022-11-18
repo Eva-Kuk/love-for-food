6aedc2a0bcac48bfab7e3b6f002ec70c7c705036
Evka K <kuklaewa@gmail.com>
2022-11-18 12:40:37 +0000

    add assign Vendors to Order

--- orders/views.py
+++ orders/views.py
@@ -17,6 +17,13 @@ def place_order(request):
     if cart_count <= 0:
         return redirect('marketplace')
     
+    vendors_ids = []
+    for i in cart_items:
+        if i.fooditem.vendor.id not in vendors_ids:
+            vendors_ids.append(i.fooditem.vendor.id)
+    print(vendors_ids)
+        
+    
     subtotal = get_cart_amounts(request)['subtotal']
     total_tax = get_cart_amounts(request)['tax']
     grand_total = get_cart_amounts(request)['grand_total']
@@ -42,6 +49,7 @@ def place_order(request):
             order.payment_method = request.POST['payment_method']
             order.save() # order id/pk is generated 
             order.order_number = generate_order_number(order.id)
+            order.vendors.add(*vendors_ids)
             order.save()
             context = {
                 'order': order,
