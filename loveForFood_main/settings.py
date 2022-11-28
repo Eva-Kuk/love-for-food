7e60b97d524ef7ad24c84ca6d77a0b5ffa9a5b8c
Evka K <kuklaewa@gmail.com>
2022-11-28 23:14:32 +0000

    deploy to Linode server

--- loveForFood_main/settings.py
+++ loveForFood_main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.162.162.140', '127.0.0.1','loveforfood.ml', 'www.loveforfood.ml']
 
 
 # Application definition
